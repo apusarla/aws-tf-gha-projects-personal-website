7bf8ed7496e4f90b9c65d56b246cfece5c9480f7
Anand Pusarla <a.pusarla@gmail.com>
2026-05-19 03:02:37 +0530

    added acm certificate

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,3 +34,20 @@ resource "aws_s3_bucket_public_access_block" "static_site_access" {
     block_public_policy     = true
     restrict_public_buckets = true
 }
+
+
+## to create a certificate
+resource "aws_acm_certificate" "krp" {
+  domain_name = "krp.com"
+  validation_method = "DNS"
+  subject_alternative_names = ["*.krp.com", "krishiv.com", "*.krishiv.com", "krishivrajpusarla.com"]
+
+  tags = {
+    Name = "krp.com"
+  }
+
+}
+
+output "certificate_arn" {
+  value = aws_acm_certificate.krp.arn
+}
