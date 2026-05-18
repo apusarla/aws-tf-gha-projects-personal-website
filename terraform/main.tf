076eb8d0bb6563f8de71448a98229147ac6ad602
Anand Pusarla <a.pusarla@gmail.com>
2026-05-18 22:49:12 +0530

    creating s3 bucket with terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,7 @@
-resource "aws_s3_bucket" "static_site" {
-  bucket = var.bucket_name
+resource "random_id" "suffix" {
+  byte_length = 4
 }
 
+resource "aws_s3_bucket" "static_site_bucket" {
+  bucket = "${var.bucket_name}-${random_id.suffix.hex}"
+}
